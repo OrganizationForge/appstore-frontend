001813efa638534d778352012c30d7297b94bacd
MatiasGiraudo <mgiraudo@gmssa.com.ar>
2024-01-03 16:44:36 -0300

    Agrego dependecia de bootstrap para la store-app

--- apps/store-app/src/app/app.component.ts
+++ apps/store-app/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { HeaderComponent } from "@angular-monorepo/header";
-import { CardComponent } from "@angular-monorepo/card";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule,HeaderComponent, CardComponent],
+  imports: [NxWelcomeComponent, RouterModule],
   selector: 'angular-monorepo-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
