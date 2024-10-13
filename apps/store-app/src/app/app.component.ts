46ebb59fd7f8bb83c6c48e86b1c999b3142be512
MatiasGiraudo <maty.giraudo@hotmail.com>
2024-10-13 19:09:17 -0300

    Agrego credenciales a las peticiones http

--- apps/store-app/src/app/app.component.ts
+++ apps/store-app/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { FooterComponent } from '@angular-monorepo/footer';
 import { HeaderComponent } from '@angular-monorepo/header';
 import { ToastUiComponent } from "@angular-monorepo/shared/ui/toast-ui";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, HeaderComponent, FooterComponent, ToastUiComponent],
+  imports: [ RouterModule, HeaderComponent, FooterComponent, ToastUiComponent],
   selector: 'angular-monorepo-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
